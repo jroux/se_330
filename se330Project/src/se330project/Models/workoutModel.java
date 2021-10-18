15d21f7d8a866deeae5975ed73805fc3c81bdd5a
jroux <jroux@chapman.edu>
2021-10-18 09:21:19 -0700

    Made edits to model

--- se330Project/src/se330project/Models/workoutModel.java
+++ se330Project/src/se330project/Models/workoutModel.java
@@ -10,18 +10,18 @@ package se330project.Models;
 // * @author jrrou
 // */
 public class workoutModel {
-//    private int time;
-//    private String type;
-//    private String equipment;
-//    
-//    public int getTime(){
-//        return time;
-//    }
-//    
-//    public void setTime(int time){
-//        this.time = time;
-//    }
-//    
+    private int time;
+    private String type;
+    private String equipment;
+    
+    public int getTime(){
+        return time;
+    }
+    
+    public void setTime(int time){
+        this.time = time;
+    }
+    
 //    public String getType(){
 //    return type;
 //    }
@@ -40,4 +40,38 @@ public class workoutModel {
 //    }
 }
 
-
+//import javafx.beans.property.*;
+//public class workoutModel {
+//    private IntegerProperty time;
+//    //private String type;
+//    //private String equipment;
+//    
+//    public workoutModel(int time){
+//        this.time = new SimpleIntegerProperty(time);
+//    }
+//    
+//    public int getTime(){
+//        return time.get();
+//    }
+//    
+//    public IntegerProperty setTime(){
+//        return time;
+//    }
+    
+//    public String getType(){
+//    return type;
+//    }
+//    
+//    public void setType(String type){
+//        this.type = type;
+//    }
+//    
+//    
+//    public String getEquipment(){
+//    return equipment;
+//    }
+//    
+//    public void setEquipment(String equipment){
+//        this.equipment = equipment;
+//    }
+//}
